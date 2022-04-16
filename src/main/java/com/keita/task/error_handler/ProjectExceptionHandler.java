e000b89bac86626c9ce3d61f794f5f5283e6b494
Mohamed keita <keitamohamed12@gmail.com>
2022-04-16 13:20:33 -0400

    Delete, find and update method

--- src/main/java/com/keita/task/error_handler/ProjectExceptionHandler.java
+++ src/main/java/com/keita/task/error_handler/ProjectExceptionHandler.java
@@ -5,16 +5,16 @@ import org.springframework.validation.BindingResult;
 
 import javax.servlet.http.HttpServletResponse;
 
-public class ProjectFound extends RuntimeException{
+public class ProjectExceptionHandler extends RuntimeException{
 
-    public ProjectFound() {
+    public ProjectExceptionHandler() {
     }
 
     public void projectIdentifierException(BindingResult bindingResult, HttpServletResponse response, String message) {
         MessageHandler.bindingResult(response, message);
     }
 
-    public ProjectFound(HttpStatus status, HttpServletResponse response, String message) {
+    public ProjectExceptionHandler(HttpStatus status, HttpServletResponse response, String message) {
         MessageHandler.projectIdentifierResponse(status, response, message);
     }
 }
--- src/main/java/com/keita/task/service/ProjectService.java
+++ src/main/java/com/keita/task/service/ProjectService.java
@@ -1,7 +1,7 @@
 package com.keita.task.service;
 
 import com.keita.task.error_handler.InvalidInput;
-import com.keita.task.error_handler.ProjectFound;
+import com.keita.task.error_handler.ProjectExceptionHandler;
 import com.keita.task.model.Project;
 import com.keita.task.repository.ProjectRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +10,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class ProjectService {
@@ -25,17 +31,71 @@ public class ProjectService {
         if (result.hasErrors()) {
             throw new InvalidInput(result, response, "Invalid fields");
         }
+        findByIdentifier(project.getIdentifier(), response);
 
-        try {
-            project.setIdentifier(project.getIdentifier().toUpperCase());
-            projectRepo.save(project);
-        }
-        catch (Exception e) {
-            throw new ProjectFound(
-                    HttpStatus.FOUND,
-                    response,
-                    "Project with identifier " + project.getIdentifier().toUpperCase() + " already exists"
-            );
+        project.setIdentifier(project.getIdentifier().toUpperCase());
+        projectRepo.save(project);
+    }
+
+    public List<Project> findAllProject(HttpServletResponse response) {
+        return StreamSupport.stream(
+                        Spliterators.spliteratorUnknownSize(projectRepo.findAll().iterator(),
+                                Spliterator.ORDERED),
+                        false)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), result -> {
+                    if (result.isEmpty()) {
+                        throw new ProjectExceptionHandler(
+                                HttpStatus.NO_CONTENT,
+                                response,
+                                "No content to load."
+                        );}
+                    return result;
+                }));
+    }
+
+    public void deleteProjectByIdentifier(String identifier, HttpServletResponse response) {
+        String message = String.format("Project with identifier %s does not exists", identifier);
+        Optional<Project> findProject = findProjectByIdentifier(HttpStatus.NO_CONTENT, identifier, response, message);
+        findProject.ifPresent(projectRepo::delete);
+        throw new ProjectExceptionHandler(HttpStatus.OK, response, String.format("Project with identifier %s was deleted", identifier));
+
+    }
+
+    public void updateProject(Project project, HttpServletResponse response) {
+        String message = String.format("Project with identifier %s does not exists", project.getIdentifier());
+        Optional<Project> findProject = findProjectByIdentifier(HttpStatus.NO_CONTENT, project.getIdentifier(), response, message);
+
+        findProject.ifPresent(result -> {
+            result.setDescription(project.getDescription());
+            result.setName(project.getName());
+            result.setStartDate(project.getStartDate());
+            result.setEndDate(project.getEndDate());
+            result.setUpdatedAt(project.getUpdatedAt());
+        });
+
+        projectRepo.save(findProject.get());
+        throw new ProjectExceptionHandler(HttpStatus.OK, response, String.format("Project with identifier %s was updated", project.getIdentifier()));
+    }
+
+    public Optional<Project> findProjectByIdentifier(
+            HttpStatus status,
+            String projectIdentifier,
+            HttpServletResponse response,
+            String passMessage) {
+        String message = !passMessage.isEmpty() ? passMessage : "Project with identifier " + projectIdentifier.toUpperCase() + " does not exists";
+        return projectRepo.findProjectByIdentifier(projectIdentifier)
+                .map(Optional::of)
+                .orElseThrow( () -> new ProjectExceptionHandler(
+                        status,
+                        response,
+                        message
+                ));
+    }
+
+    private void findByIdentifier(String projectIdentifier, HttpServletResponse response) {
+        String message = "Project with identifier " + projectIdentifier.toUpperCase() + " already exist";
+        if (projectRepo.findProjectByIdentifier(projectIdentifier).isPresent()){
+            throw new ProjectExceptionHandler(HttpStatus.FOUND, response, message);
         }
     }
 }
