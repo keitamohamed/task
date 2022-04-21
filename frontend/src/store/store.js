22a3d09a2bf8fa66e1dee1821e437b40727aead0
Mohamed keita <keitamohamed12@gmail.com>
2022-04-21 13:56:42 -0400

    Fix bug

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,10 +2,10 @@ import {configureStore} from '@reduxjs/toolkit'
 import thunk from "redux-thunk";
 
 import project_slice from "./project_slice";
-import task_slice from "./task_slice";
+import taskSlice from "./task_slice";
 
 const store = configureStore({
-    reducer: {project: project_slice.reducer, task: task_slice.reducer},
+    reducer: {project: project_slice.reducer, task: taskSlice.reducer},
     middleware: [thunk]
 })
 
