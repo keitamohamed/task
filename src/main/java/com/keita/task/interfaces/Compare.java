a422e8ba7d46fe55e2dc7c26317fc0bc5d5d8770
Mohamed keita <keitamohamed12@gmail.com>
2022-05-08 02:06:41 -0400

    Refactor

--- src/main/java/com/keita/task/interfaces/Compare.java
+++
@@ -1,9 +0,0 @@
-package com.keita.task.interfaces;
-
-import com.keita.task.model.ProjectTask;
-
-import java.util.Comparator;
-
-public interface Compare extends Comparator<ProjectTask> {
-    int compare(ProjectTask first, ProjectTask second);
-}
