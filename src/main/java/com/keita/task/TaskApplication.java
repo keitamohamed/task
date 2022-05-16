9d5c20d5ceb26246d251ba7353483bf466b82672
Mohamed keita <keitamohamed12@gmail.com>
2022-05-16 11:42:38 -0400

    Security configuration

--- src/main/java/com/keita/task/TaskApplication.java
+++ src/main/java/com/keita/task/TaskApplication.java
@@ -2,8 +2,10 @@ package com.keita.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TaskApplication {
 
     public static void main(String[] args) {
